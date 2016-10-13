7c458d5b1285574ced9588138748c97b126ec582
shraddhakarmali <skarmali@gmail.com>
2016-10-12 22:18:49 -0400

    CORS - draft - doesnt work

--- ResourceManager/Controllers/UserController.cs
+++ ResourceManager/Controllers/UserController.cs
@@ -9,9 +9,11 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using ResourceManager.Models;
+using System.Web.Http.Cors;
 
 namespace ResourceManager.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UsersController : ApiController
     {
         // GET: api/User
